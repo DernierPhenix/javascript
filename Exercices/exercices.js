f06eb3b837ea2cc98db489aaec22ebbc4c7fca6e
DernierPhenix <jpierre.fleury75@gmail.com>
2022-06-03 09:35:25 +0200

    Exercice du magicien

--- Exercices/exercices.js
+++ Exercices/exercices.js
@@ -1,5 +1,15 @@
-if (confirm('Souhaitez-vous vraiment supprimer cet exercice ?')){
-    alert('Exercice supprimé !');
-}else{
-    alert('Exercice non supprimé !');
-};
+function abracadabra() {
+  let prenom = prompt("Quel est votre prénom ?");
+  let nom = prompt("Quel est votre nom ?");
+  let age = prompt("Quel est votre âge ?");
+  alert(
+    "Sapristi ! On ne m'avait pas prévenu que c'était vous, " +
+      prenom +
+      " ! Euh... Je veux dire... Monsieur le grand magicien " +
+      nom +
+      " ! Cela fait déjà " +
+      age +
+      " ans que vous faites rayonner notre contrée ! Vous êtes vraiment un vrai magicien ! "
+  );
+}
+abracadabra();
